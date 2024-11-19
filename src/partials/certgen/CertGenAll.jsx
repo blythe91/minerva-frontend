27dcea2103a9315a049f05212da3e3c5f7f25cfc
Oscar Giovanni Castro Contreras <castro.oscar18@gmail.com>
2024-11-19 15:09:11 -0400

    feat (CertGenAll): corrige la impresión masiva de certificados en PDF

--- src/partials/certgen/CertGenAll.jsx
+++ src/partials/certgen/CertGenAll.jsx
@@ -1,156 +1,11 @@
 import React, { useState } from 'react';
 import { Page, Text, View, Document, StyleSheet, PDFDownloadLink, Image, pdf } from '@react-pdf/renderer';
 import { Api } from '../../services/Api';
-import { useParams } from 'react-router-dom';
+import { showAlertTopEnd, showAlert } from '../../components/utils/Alert'; // alertas
+import { useNavigate, useParams } from 'react-router-dom';
+const navigate = useNavigate();
 import { saveAs } from 'file-saver';
-import backgroundImage from '/cert_template.jpg';
-import { Font } from '@react-pdf/renderer';
-
-// Registrar la fuente Roboto
-Font.register({
-  family: 'Roboto',
-  src: '/fonts/Roboto-Regular.ttf', // Reemplaza con la ruta correcta de tu archivo de fuente
-});
-// Registrar la fuente Roboto
-Font.register({
-  family: 'Roboto',
-  src: '/fonts/Roboto-Bold.ttf', // Reemplaza con la ruta correcta de tu archivo de fuente
-  fontWeight: 'bold',
-    fontFamily: 'Roboto',
-});
-Font.register({
-  family: 'RobotoBoldCondensed',
-  src: '/fonts/Roboto-BoldCondensed.ttf', // Reemplaza con la ruta correcta de tu archivo de fuente
-});
-
-// const styles = StyleSheet.create({
-//   page: {
-//     backgroundColor: '#ffffff',
-//     padding: 2,
-//     position: 'relative',
-//   },
-//   background: {
-//     position: 'absolute',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//     zIndex: -1,
-//     opacity: 1,
-//   },
-//   section: {
-//     marginTop: 3,
-//     marginBottom: 3,
-//     marginLeft: 6,
-//     marginRight: 6,
-//     padding: 6,
-//   },
-//   coordinationName: {
-//     fontSize: 12,  // Ajusté el tamaño de la fuente a 12
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'left',
-//     marginBottom: 10,
-//     marginTop: 85,
-//     marginLeft: 6,  // Corregí el margen izquierdo
-//     paddingLeft: 240,
-//     textTransform: 'uppercase',
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   certificateTitle: {
-//     fontSize: 42,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     marginTop: 20,
-//     marginBottom: 0,
-//     textTransform: 'uppercase',
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   title: {
-//     fontSize: 12,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     marginBottom: 15,
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   participantName: {
-//     fontSize: 28,
-//     fontStyle: 'italic',
-//     color: '#996515', // Dorado
-//     textAlign: 'center',
-//     marginBottom: 10,
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   idNumber: {
-//     fontSize: 12,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     marginBottom: 5,
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//     marginBottom: 20
-//   },
-//   eventName: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#5db6fa', // Azul claro
-//     textAlign: 'center',
-//     marginBottom: 10,
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   modalityAndDuration: {
-//     fontSize: 12,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   dateLineText: {
-//     fontSize: 12,
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-//   },
-//   preambleEventName: {
-//     fontSize: 12,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     textAlign: 'center',
-//     width: '100%',  // Asegura que ocupe todo el ancho disponible
-
-//   },
-//   eventPrefix: {
-//     fontSize: 10,
-//     fontWeight: 'bold',
-//     fontFamily: 'Roboto',
-//     color: '#050a30', // Azul oscuro
-//     position: 'absolute',
-//     bottom: 8,
-//     right: 160,
-//     width: 'auto',  // Evita que el texto ocupe más espacio de lo necesario
-//   },
-//   footer: {
-//     fontSize: 12,
-//     textAlign: 'center',
-//     marginTop: 20,
-//   },
-//   emptyLine: {
-//     marginBottom: 5,
-//   },
-// });
-
-
-// Componente CertGenAll
+import CertificateDocument from './CertificateDocument';
 
 
 const CertGenAll = () => {
@@ -160,15 +15,9 @@ const CertGenAll = () => {
   const [progress, setProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  const formattedDate = new Date(eventDetails?.end_date).toLocaleDateString('es-ES', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  });
-
   // Fetch de los participantes y detalles del evento (sin asincronía)
   const fetchData = () => {
-    Api.get(`/participant-events?event_id=${id}`).then(participantResponse => {
+    Api.get(`/participant-events/get-participants/${id}`).then(participantResponse => {
       console.log("/////////");
       console.log(participantResponse.data);
       console.log("/////////");
@@ -197,98 +46,36 @@ const CertGenAll = () => {
     fetchData();
   }, [id]);
 
-  // Generar el documento PDF para cada participante
-  const CertificateDocument = ({ p }) => (
-    <Document>
-      {/* Primera cara del certificado */}
-      <Page size="A4" orientation="landscape" style={styles.page}>
-        <Image src={backgroundImage} style={styles.background} />
-        
-        <View style={styles.section}>
-          <Text style={styles.coordinationName}>
-            {p.coordination_name || ''}
-          </Text>
-          <Text style={styles.certificateTitle}>Certificado</Text>
-          <Text style={styles.title}>Que se otorga a:</Text>
-          <Text style={styles.participantName}>
-            {`${p.pri_nom} ${p.seg_nom} ${p.pri_ape} ${p.seg_ape}`}
-          </Text>
-          <Text style={styles.idNumber}>
-            V.-{p.cedula}
-          </Text>
-  
-          <Text style={styles.preambleEventName}>
-            Por su participación en la actividad {p.event_type_name} <br/>
-            en calidad de {p.name_participant_type}:
-          </Text>
-          <Text style={styles.eventName}>
-            {p.name_event}
-          </Text>
-  
-          <Text style={styles.preambleEventName}>
-            facilitador: "acá va un nombre"
-          </Text>
-          
-          <Text style={styles.modalityAndDuration}>
-            Modalidad: {eventDetails?.event_modality}
-          </Text>
-          <Text style={styles.modalityAndDuration}>
-            Duración: {eventDetails?.academic_hours} horas académicas
-          </Text>
-  
-          <Text style={styles.emptyLine}>        </Text>
-  
-          <Text style={styles.dateLineText}>
-            {eventDetails?.date_line_text || ''}
-          </Text>
-  
-          <Text style={styles.dateLineText}>
-            {eventDetails?.address ? eventDetails?.address : ''}
-          </Text>
-        </View>
-  
-        <Text style={styles.footer}>{formattedDate}</Text>
-        <Text style={styles.eventPrefix}>
-            {eventDetails.event_prefix || ''}
-          </Text>
-      </Page>
-  
-      {/* Segunda cara del certificado */}
-      <Page size="A4" orientation="landscape" style={styles.page}>
-        <Image src={backgroundImage} style={styles.background} />
-        <View style={styles.section}>
-          <Text style={styles.largeTitle}>Aquí va el programa de la actividad</Text>
-        </View>
-      </Page>
-    </Document>
-  );
-  
-
-  const downloadCertificate = (participant) => {
-    pdf(<CertificateDocument p={participant} />)
-      .toBlob()
-      .then(pdfBlob => {
-        const fileName = `${participant.cedula}_${eventDetails.event_prefix}_${participant.pri_nom}_${participant.pri_ape}_certificado.pdf`;
-        saveAs(pdfBlob, fileName);
-      })
-      .catch(error => {
-        console.error('Error generando el PDF:', error);
-        alert('Hubo un error al generar el certificado.');
-      });
-  };
-  
-
-  // Generar y descargar certificados automáticamente de manera secuencial
   const generateAllCertificates = async () => {
-    setIsLoading(true);
-    for (let i = 0; i < participantEvent.length; i++) {
-      await downloadCertificate(participantEvent[i]); // Espera a que se genere cada certificado
-      setProgress(((i + 1) / participantEvent.length) * 100);
-    }
-    setIsLoading(false);
-    alert('Todos los certificados han sido generados y descargados.');
+      setIsLoading(true);
+      for (let i = 0; i < participantEvent.length; i++) {
+          try {
+              await pdf(
+                  <CertificateDocument
+                      backgroundImage={`/certificate_templates/${eventDetails.certificate_template_name}`}
+                      signature1={`/signatures/${eventDetails.image_signature1}`}
+                      signature2={`/signatures/${eventDetails.image_signature2}`}
+                      signature3={`/signatures/${eventDetails.image_signature3}`}
+                      participantEvent={participantEvent[i]}
+                      eventDetails={eventDetails}
+                  />
+              )
+                  .toBlob()
+                  .then(pdfBlob => {
+                      const fileName = `Certificado_${eventDetails.event_prefix}_${participantEvent[i].cedula}_${participantEvent[i].pri_nom}_${participantEvent[i].pri_ape}.pdf`;
+                      saveAs(pdfBlob, fileName);
+                  });
+          } catch (error) {
+              console.error('Error generando el certificado:', error);
+              showAlert('Error', 'Error generando el certificado.', 'error');
+
+          }
+          setProgress(((i + 1) / participantEvent.length) * 100);
+      }
+      setIsLoading(false);      
+      showAlert('Éxito', 'Todos los certificados han sido generados y descargados con éxito.', 'success');
+      navigate(`/events/${id}/certificates/`);
   };
-  
 
   // Mostrar la barra de progreso
   const ProgressBar = () => (
